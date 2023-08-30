95e2b0517980b4ada77e8af0e3ac32f0f4065ec5
Michael Wilcox <tuk44976@temple.edu>
2023-08-30 11:21:42 -0400

    Implemented profile picture path and url functionality to the Syllabus Page. The current implementation cannot read the avatar provided by the API, but it does read jpg/png files
    Also fixed a bug where the syllabus section would not vertically resize with window.
    
    Discord links are now displayed. If a syllabus has no discord channel, the field now says none instead of null.

--- src/SyllabusPage.java
+++ src/SyllabusPage.java
@@ -1,15 +1,20 @@
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.Initializable;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.ColumnConstraints;
+
+import javax.imageio.ImageIO;
 
 public class SyllabusPage implements Initializable {
 
@@ -21,16 +26,32 @@ public class SyllabusPage implements Initializable {
     private GridPane syllabusPane;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Main main = new Main();
+        try {
+            User user = main.getUserInfo();
+            greetingLabel.setText("Hello, " + user.name + "!");
+            Image profileImage = new Image("default_image.jpg");
 
-        greetingLabel.setText("Hello, "/*insertNameHere*/);
+            if (user.profile_photo_path != null) {
+                profileImage = new Image(user.profile_photo_path);
 
-        Image profileImage = new Image("default_image.jpg");
-
-        if (false/*profile picture is not null*/) {
-            //profilePicture.setImage(userProfilePicture);
-        } else {
+            } else if (user.profile_photo_url != null) {
+                URL photoURL = new URL(user.profile_photo_url);
+                try {
+                    BufferedImage image = ImageIO.read(photoURL);
+                    if (image != null) {
+                        profileImage = SwingFXUtils.toFXImage(image, null);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             profilePicture.setImage(profileImage);
+        } catch (Exception e) {
+            greetingLabel.setText("Hello!");
+            e.printStackTrace();
         }
+
         /* sample data
         String[] syllabus1 = {"1", "Projects in Computer Science", "Spring", "2023", "2023-01-17", "2023-05-09"};
         String[] syllabus2 = {"2", "Projects in Computer Science 002", "Spring", "2023", "2023-01-18", "2023-05-09"};
@@ -38,7 +59,7 @@ public class SyllabusPage implements Initializable {
         String[] syllabus4 = {"2", "Projects in Computer Science 004", "Spring", "2023", "2023-01-18", "2023-05-09"};
 
         String[][] syllabi = {syllabus1, syllabus2, syllabus3, syllabus4}; */
-        Main main = new Main();
+
         try {
             SyllabusResponse syllabusResponseObject = main.getSyllabi();
             int i = 0;
@@ -46,8 +67,23 @@ public class SyllabusPage implements Initializable {
                 //add additional relevant labels when they arrive
                 Label title = new Label("Course: " + syllabus.course_name);
                 Label semester = new Label("Semester: " + syllabus.course_semester + " " + syllabus.course_year);
-                Label startEndDates = new Label("Date: " + syllabus.start_date + " - " + syllabus.end_date);
-                Label discord = new Label("Discord Channel: " + syllabus.discord_channel);
+                Label startEndDates = new Label("Duration: " + syllabus.start_date + " - " + syllabus.end_date);
+                boolean isLink = true;
+                Label discordLabel = new Label("Discord Channel: ");
+                Label discord = new Label();
+                Hyperlink discordLink = new Hyperlink();
+                if (syllabus.discord_channel == null) {
+                    isLink = false;
+                    discord.setText("None");
+                    discord.setWrapText(true);
+                    discord.setMaxWidth(210);
+                } else {
+                    discordLink.setText("https://discord.com/channel/" + syllabus.discord_channel);
+                    discordLink.setWrapText(true);
+                    discordLink.setMaxWidth(210);
+                }
+
+
                 Button selectSyllabus = new Button("Select");
 
                 title.setWrapText(true);
@@ -56,8 +92,7 @@ public class SyllabusPage implements Initializable {
                 semester.setMaxWidth(210);
                 startEndDates.setWrapText(true);
                 startEndDates.setMaxWidth(210);
-                discord.setWrapText(true);
-                discord.setMaxWidth(210);
+
 
                 GridPane grid = new GridPane();
                 grid.getColumnConstraints().add(new ColumnConstraints(210));
@@ -65,7 +100,12 @@ public class SyllabusPage implements Initializable {
                 grid.add(title, 0, 0);
                 grid.add(semester, 0, 1);
                 grid.add(startEndDates, 0, 2);
-                grid.add(discord, 0, 3);
+                grid.add(discordLabel, 0, 3);
+                if (isLink) {
+                    grid.add(discordLink, 0, 4);
+                } else {
+                    grid.add(discord, 0, 4);
+                }
                 grid.add(selectSyllabus, 1, 0);
 
                 if (i % 2 == 0) {
